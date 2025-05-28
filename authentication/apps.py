ef85c8bf6b2f3ded5139fb0684bb33a26142e1ac
cletus ola <cletushunsu@gmail.com>
2025-05-28 15:37:33 +0100

    configured signals with app

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+    # configure signal
+    def ready(self):
+        import authentication.signals
