f1c9c16b5ba4533b21a66dc7e2e288d3d4a232f2
cletus ola <cletushunsu@gmail.com>
2025-05-29 07:27:31 +0100

    created profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+# profile model 
+class Profile(models.Model):
+    fullname = models.CharField(max_length=80, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.user
+    
+    class Meta:
+        ordering = ['-created_at']
