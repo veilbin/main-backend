c9eeb7f2c7ecf9fb08ff7ed860c9c5c36f80efb7
cletus ola <cletushunsu@gmail.com>
2025-05-29 08:13:09 +0100

    created file models

--- files/models.py
+++ files/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+# file model 
+class File(models.Model):
+    filename = models.CharField(max_length=150, null=False, blank=False)
+    file = models.FileField(upload_to="files/")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField(null=False, blank=False)
+
+    class Meta:
+        ordering = ['-uploaded_at']
+
+    def __str__(self):
+        return f"{self.owner}-{self.filename}"
+
