010a5678a02bb569a4e98260d9b60388f249d9bf
cletus ola <cletushunsu@gmail.com>
2025-05-30 11:14:19 +0100

    created files serializers

--- files/serializers.py
+++ files/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-
 from .models import File
 
 # file serializer
 class FileSeriailizer(serializers.ModelSerializer):
-    owner = serializers.CharField(required=False)
-    filename = serializers.CharField(required=True, max_lenght=150)
-    file = serializers.FileField(required=True)
-    expiration = serializers.DateTimeField(required=True)
+    share_token = serializers.CharField(max_length=200, required=False)
+    share_link = serializers.CharField(max_length=600, required=False)
     
     class Meta:
         model = File
-        fields = ['id','filename', 'file', 'owner', 'uploaded_at', 'expiration']
+        fields = ['id','filename', 'file', 'owner', 'is_shareable', 'share_token', 'share_link', 'uploaded_at', 'expiration']
+        read_only_fields = ['uploaded_at', 'share_token', 'share_link']
+
+    
