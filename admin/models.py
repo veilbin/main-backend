6c58098d4db3e39c7784ef0315316548cc8dfc5d
cletus ola <cletushunsu@gmail.com>
2025-06-01 16:20:22 +0100

    created Deletion Schedule model

--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+# accounts sechduled for deletion 
+class DeletionSchedule(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    request_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user}"
+    
+    
