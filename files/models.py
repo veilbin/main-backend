4ed5b5f5f46a067981ca7059fcb4eae0637c31c9
cletus ola <cletushunsu@gmail.com>
2025-06-06 10:04:43 +0100

    Removed share_link field and function

--- files/models.py
+++ files/models.py
@@ -1,7 +1,6 @@
 import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
 User = get_user_model()
 
@@ -11,23 +10,22 @@ class File(models.Model):
     file = models.FileField(upload_to="files/")
     is_shareable = models.BooleanField(default=True)
     share_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-    share_link = models.URLField(max_length=600, blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     expiration = models.DateTimeField(null=False, blank=False)
 
-    def save(self, *args, **kwargs):
-        # generate share link if file is shareable 
-        if self.is_shareable:
-            base_url = "http://locachost:8080"
-            self.share_link = f"{base_url}{reverse('file-share', kwargs={'share_token':str(self.share_token)})}"
-        else:
-            self.share_link = None 
+    # def save(self, *args, **kwargs):
+    #     # generate share link if file is shareable 
+    #     if self.is_shareable:
+    #         base_url = os.getenv('HOTSNAME')
+    #         self.share_link = f"{base_url}{reverse('file-share', kwargs={'share_token':str(self.share_token)})}"
+    #     else:
+    #         self.share_link = None 
 
-        super().save(*args, **kwargs)
+    #     super().save(*args, **kwargs)
 
-    class Meta:
-        ordering = ['-uploaded_at']
+    # class Meta:
+    #     ordering = ['-uploaded_at']
 
     def __str__(self):
         return f"{self.filename}-{self.share_token}"
