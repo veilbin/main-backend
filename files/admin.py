ad535d69e564c8d0188ec46c7c4a0336caade7e6
cletus ola <cletushunsu@gmail.com>
2025-06-06 10:16:30 +0100

    created FileAdmin class

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import File
 
-# Register your models here.
+# file admin 
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'is_shareable', 'share_token', 'owner', 'uploaded_at', 'expiration']
+    list_display_links = ['filename','is_shareable', 'owner']
+    list_filter = ['is_shareable']
+    search_fields = ['filename', 'owner']
+    ordering = ['-expiration']
