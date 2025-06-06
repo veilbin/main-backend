4395c1a2c3e55d5632cfd090bd6351c4726eed89
cletus ola <cletushunsu@gmail.com>
2025-06-06 10:28:43 +0100

    creation deletion schedule serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from .models import Profile
+from .models import Profile, DeletionSchedule
 
 User= get_user_model()
 
@@ -13,3 +13,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id', 'fullname', 'user', 'created_at', 'updated_at']
+
+# deletion schedule 
+class DeletionSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(required=False)
+
+    class Meta:
+        models = DeletionSchedule 
+        feilds = ['id', 'user', 'request_date', 'approved'] 
